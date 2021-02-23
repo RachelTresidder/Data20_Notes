f161db89f6f52eed7177024bb735b0e02e0182c9
Rachel Tresidder <rachel.tresidder@gmail.com>
2021-02-23 14:31:14 +0000

    makinfgf the simulator more advanced by moving round a hypothetical grid

--- driving car.py
+++ driving car.py
@@ -8,16 +8,31 @@
 #     pass
 # this skips the function
 
-def speed():
-    mph = input("How fast would you like to go? Choose a number from 1 to 50 mph")
-    return mph
+
+import random
+# from math import *
+
+
+def random_destination():
+    random_coordinates = [0, 0]
+    random_coordinates[0] = random.randint(-50, 50)
+    random_coordinates[1] = random.randint(-50, 50)
+    return random_coordinates
+
+
+# print(random_destination())
+
+
+def distance():
+    meters = input("How fast would you like to go? Choose a number from 1 to 20 meters ")
+    return meters
 # this is the up value
 
 # print(speed())
 
 
 def direction():
-    choice = input("Which direction would you like to go in? Choose either UP or DOWN").lower()
+    choice = input("Which direction would you like to go in? Choose either UP or DOWN ").lower()
     direction_counter = 0
     if choice == "up":
         direction_counter += 1
@@ -29,7 +44,7 @@ def direction():
 
 
 def turn():
-    way = input("Which way would you like to turn? Choose either LEFT, RIGHT, or STRAIGHT").lower()
+    way = input("Which way would you like to turn? Choose either LEFT, RIGHT, or STRAIGHT ").lower()
     turn_counter = 0
     if way == "left":
         turn_counter -= 1
@@ -38,27 +53,35 @@ def turn():
     else:
         turn_counter += 1
     return turn_counter
-# across value in here
-
-# this is not working with positive and negative values
 
 
-def main():
-    destination = [30, 50]
+def final_destination():
+    destination = random_destination()
     print(destination)
-    coordinates = [0,0]
+    coordinates = [0, 0]
     while coordinates != destination:
-        if coordinates == destination:
-            print("You have arrived")
-        else:
             print("You haven't reached your destination, keep going")
-            print(f"these are your current coordinates: {coordinates}")
-            coordinates[0] = int(coordinates[0] + (int(turn()) * int(speed())))
-            coordinates[1] = int(coordinates[1] + (int(direction()) * int(speed())))
-
-print(main())
-
-# not subtracting negatives
+            print(f"these are your current coordinates: {coordinates} \n")
+            coordinates[0] = int(coordinates[0] + int((int(turn()) * int(distance()))))
+            coordinates[1] = int(coordinates[1] + int((int(direction()) * int(distance()))))
+    if coordinates == destination:
+        print("You have arrived")
 
 
+print(final_destination())
 
+
+# def temperature():
+#     temp_dest = (int(destination[0]) + int(destination[1])) ^ 2
+#     temp_coord = (int(coordinates[0]) + int(coordinates[1])) ^ 2
+#     difference = sqrt(int(temp_dest + temp_coord))
+#     if difference < 20:
+#         return("Hot!")
+#     elif difference > 20 and difference < 50:
+#         return("warm")
+#     else:
+#         return("cold..... :( ")
+
+# need a while loop here too
+
+# ((x - x2) ^2) + ((y - y2)) ^2 = sqrt(x3 + y3)
