71ce79fd10e0be4e82a91ae0c0735ccd2644f93f
Rachel Tresidder <rachel.tresidder@gmail.com>
2021-02-25 16:44:16 +0000

    placed the final main function inside the class

--- imbd class practice.py
+++ imbd class practice.py
@@ -7,27 +7,25 @@ class ETL:
         self.header = [['TitleType', 'Title', 'StartYear', 'RunTimeMins', 'Genres']]
         self.movies_shows = []
         self.movie_genres = []
-
+        self.csv = []
 
     def extract(self):
         with open(self.csvfile) as file:
             readcsv = csv.reader(file, delimiter=',')
             next(readcsv)
-            return list(readcsv)
+            self.csv = list(readcsv)
+            return self.csv
 
-
-    def transform_movies_shows(self, readcsv):
-        for i in readcsv:
+    def transform_movies_shows(self):
+        for i in self.csv:
             if i[0] == 'movie' or i[0] == 'tvseries':
                 self.movies_shows.append(i)
         return self.movies_shows
-
 
     def transform_secondary_genres(self):
         for i in self.movies_shows:
             i[7] = i[7].split(',', 1)[0]
         return self.movies_shows
-
 
     def transform_remove(self):
         for i in self.movies_shows:
@@ -36,7 +34,6 @@ class ETL:
             i.pop(2)
         return self.movies_shows
 
-
     def loading_csv(self, new_file_name):
         self.header.extend(self.movies_shows)
         with open(new_file_name, 'w') as file:
@@ -44,17 +41,17 @@ class ETL:
             writer.writerows(self.header)
 
 
-def main(old_file_name, new_file_name):
-    Example = ETL()
-    Example.csvfile = old_file_name
-    a = Example.extract()
-    Example.transform_movies_shows(a)
-    Example.transform_secondary_genres()
-    Example.transform_remove()
-    Example.loading_csv(new_file_name)
+    def main(old_file_name, new_file_name):
+        Example = ETL()
+        Example.csvfile = old_file_name
+        Example.extract()
+        Example.transform_movies_shows()
+        Example.transform_secondary_genres()
+        Example.transform_remove()
+        Example.loading_csv(new_file_name)
 
 
-main('imdbtitles.csv', 'newimdb.csv')
+ETL.main('imdbtitles.csv', 'newimdb.csv')
 
 
 
